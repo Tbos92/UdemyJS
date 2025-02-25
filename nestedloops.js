52057e03e6b9b42194845657e2e2a26d3da107e2
Tbos92 <tbos92@gmail.com>
2025-02-25 09:22:05 -0500

    Fixed error in initial nested loop where the inner loop was checking i instead of j as its condition and therefore looping infinitely. Also added a more practical example of a nested for loop using a seating chart (i.e. rows and columns in effect)

--- nestedloops.js
+++ nestedloops.js
@@ -1,6 +1,34 @@
-for (let i = 1; i <= 10; i++) {
-  console.log(`i is ${i}`);
-  for (let j = 1; i <= 3; j++) {
-    console.log(`   j is ${j}`);
+function simpleNestedForLoop() {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`i is ${i}`);
+    for (let j = 1; j <= 3; j++) {
+      console.log(`   j is ${j}`);
+    }
   }
 }
+
+// Nested loops are good for "traversing" across nested arrays
+
+/* i here loops over the seatingChart array such that i = 0 would be
+the first subarray, i = 1 would be the 2nd subarray, etc. To loop over
+the subarrays then you would need a nested loop. To do so, loop over
+seatingChart[i] */
+
+function loopOverSeatingChart() {
+  const seatingChart = [
+    ["Kristen", "Erik", "Namita"],
+    ["Geoffrey", "Juanita", "Antonio", "Kevin"],
+    ["Yuma", "Sakura", "Jack", "Erika"],
+  ];
+
+  for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`Row #${i + 1}`);
+
+    for (let j = 0; j < row.length; j++) {
+      console.log(`   ${row[j]}`);
+    }
+  }
+}
+
+loopOverSeatingChart();
